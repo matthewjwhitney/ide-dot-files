afa4961506ab68cf6fdb7ec0caf5b9abba7b054e
Matthew J. Whitney <mwhitney@agoraaltx.com>
2019-09-30 20:54:39 -0700

    vscode keybindings, settings, and plugins

---
+++ vscode/keybindings.js
@@ -0,0 +1,34 @@
+// Place your key bindings in this file to overwrite the defaults
+[
+    { "key": "ctrl+f", "command": "actions.find" },
+    { "key": "ctrl+h", "command": "editor.action.startFindReplaceAction" },
+    { "key": "ctrl+a", "command": "editor.action.selectAll" },
+    {
+        "key": "ctrl+h",
+        "command": "workbench.action.focusSideBar"
+    },
+    {
+        "key": "ctrl+l",
+        "command": "workbench.action.focusFirstEditorGroup"
+    },
+    {
+        "key": "ctrl+1",
+        "command": "-workbench.action.focusFirstEditorGroup"
+    },
+    {
+        "key": "ctrl+k",
+        "command": "workbench.action.nextEditor"
+    },
+    {
+        "key": "ctrl+pagedown",
+        "command": "-workbench.action.nextEditor"
+    },
+    {
+        "key": "ctrl+j",
+        "command": "workbench.action.previousEditor"
+    },
+    {
+        "key": "ctrl+pageup",
+        "command": "-workbench.action.previousEditor"
+    }
+]
